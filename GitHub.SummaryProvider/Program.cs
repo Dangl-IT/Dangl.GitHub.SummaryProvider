6537b0d0635d90add2d17cab84022e270e1a2e41
Georg Dangl <georg@dangl.me>
2022-05-24 15:52:37 +0200

    Add initial Program.cs implementation

--- GitHub.SummaryProvider/Program.cs
+++ GitHub.SummaryProvider/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var repoInspector = new GitHubRepoInspector("organization",
+    "repository",
+    "branch",
+    "gitHubToken");
+var commits = await repoInspector.GetCommitsForDevelopBranchAsync();
+var pullRequests = await repoInspector.GetPullRequestDataAsync();
+
+var allActions = commits.Select(c => new RepoAction { Date = c.AuthoredDate, Content = c})
+    .Concat(pullRequests.Select(p => new RepoAction { Date = p.MergedAt, Content = p }));
+
+var summaryExporter = new SummaryExporter(allActions, "outputFilePath", 2022, 5);
+await summaryExporter.ExportSummaryAsync();
